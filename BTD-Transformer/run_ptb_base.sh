9838f2dcd8d2be6a4a9ef0fbc41e83984483e1fa
szhangtju <szhang96@tju.edu.cn>
2019-04-12 18:16:39 +0800

    initial

---
+++ BTD-Transformer/run_ptb_base.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ $1 == 'train' ]; then
+    echo 'Run training...'
+    python train.py \
+        --cuda \
+        --data ../data/ptb/ \
+        --dataset ptb \
+        --n_layer 12 \
+        --d_model 512 \
+        --n_head 8 \
+        --d_head 40 \
+        --d_inner 2100 \
+        --dropout 0.1 \
+        --dropatt 0.0 \
+        --optim adam \
+        --lr 0.00025 \
+        --warmup_step 0 \
+        --max_step 2000 \
+        --tgt_len 32 \
+        --mem_len 32 \
+        --eval_tgt_len 32 \
+        --batch_size 120 \
+        --gpu0_bsz 4 \
+        ${@:2}
+elif [ $1 == 'eval' ]; then
+    echo 'Run evaluation...'
+    python eval.py \sh
+        --cuda \
+        --data ../data/ptb/ \
+        --dataset ptb \
+        --tgt_len 32 \
+        --mem_len 32 \
+        --clamp_len 400 \
+        --same_length \
+        --split test \
+        ${@:2}
+else
+    echo 'unknown argment 1'
+fi
